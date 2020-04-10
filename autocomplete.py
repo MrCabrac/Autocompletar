d06634a81a972a9b12e08f21b2a36adfc90f7f28
MrCabrac <bmartinez.red@gmail.com>
2020-04-09 21:50:13 -0500

    Second commit

---
+++ autocomplete.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 10:09:50 2020
+
+@author: bmart
+"""
+
+import sqlite3
+from os import listdir
+from os.path import isfile, join
+import time
+import random
+import re
+
+class db():
+    def __init__(self):
+        self.dbName = "palabras.db"
+        self.tableName = "palabras"
+        self.c = 0
+        self.conn = 0
+        self.createTable()
+        
+    def createTable(self):
+        self.connectDB()
+        sql = '''CREATE TABLE IF NOT EXISTS palabras (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                      word TEXT NOT NULL,
+                                                      prob REAL NOT NULL DEFAULT 0,
+                                                      uses NUMERIC NOT NULL DEFAULT 1)'''
+        self.c.execute(sql)
+        self.conn.commit()
+        self.closeDB()
+    
+    def connectDB(self):
+        self.conn = sqlite3.connect(self.dbName)
+        self.c = self.conn.cursor()
+    
+    def closeDB(self):
+        self.c.close()
+        self.conn.close()
+
+    def insertWord(self, word):
+        #Verificar si la palabra existe
+        exist = self.getWord(word)
+        if not exist: #Si no existe la palabra
+            self.connectDB()
+            palabra = [word]
+            sql = '''INSERT INTO palabras (word) VALUES (?)'''
+            self.c.execute(sql, [palabra[0].lower()])
+            self.conn.commit()
+            self.closeDB()
+        else:
+            raise Exception("Esta palabra ya existe")
+    
+    def getWordId(self, number):
+        self.connectDB()
+        sql = "SELECT * FROM palabras WHERE id==?"
+        self.c.execute(sql, [str(number)])
+        for word in self.c:
+            print(word)
+        self.conn.commit()
+        self.closeDB()
+        return word
+    
+    def getWord(self, word):
+        self.connectDB()
+        sql = "SELECT EXISTS(SELECT * FROM palabras WHERE word = ? LIMIT 1)"
+        self.c.execute(sql, [word.lower()])
+        response = True
+        for result in self.c:
+            if result[0] == 1:
+                response = True
+            else:
+                response = False
+        self.conn.commit()
+        self.closeDB()
+        return response
+
+    def getUsesWord(self, word):
+        self.connectDB()
+        sql = "SELECT uses FROM palabras WHERE word = ?"
+        self.c.execute(sql, [word.lower()])
+        for result in self.c:
+            response = result[0]
+        self.conn.commit()
+        self.closeDB()
+        return response
+    
+    def setUsesWord(self, word, uses):
+        self.connectDB()
+        sql = "UPDATE palabras SET uses=? WHERE word=?"
+        self.c.execute(sql, [uses, word.lower()])
+        self.conn.commit()
+        self.closeDB()
+
+    def setProbWord(self, word, prob):
+        self.connectDB()
+        sql = "UPDATE palabras SET prob=? WHERE word=?"
+        self.c.execute(sql, [prob, word.lower()])
+        self.conn.commit()
+        self.closeDB()
+    
+    def getWordInitials(self, initials):
+        self.connectDB()
+        sql = "select * from palabras where word like ?"
+        self.c.execute(sql, [initials.lower()+"%"])
+        words = list()
+        for word in self.c:
+            words.append(word)
+        self.conn.commit()
+        self.closeDB()
+        return words
+    
+    def getAllWords(self):
+        self.connectDB()
+        sql = "select * from palabras"
+        self.c.execute(sql)
+        words = list()
+        for word in self.c:
+            words.append(word[1])
+            print(word)
+        self.conn.commit()
+        self.closeDB()
+        return words
+    
+class wordManage():
+    def __init__(self):
+        self.files = list()
+        
+    def readAllFiles(self):
+        self.files = [f for f in listdir("words") if isfile(join("words", f))]
+    
+    def getWordsList(self):
+        wordsList = list()
+        database = db()
+        for file in self.files:
+            with open("words/"+file, encoding = "utf-8", mode = 'r') as f:
+                text = f.readlines()
+                wordsList.append(text)
+                numberWords = 0
+        for wordList in wordsList:
+            numberWords += len(wordList)
+        print("Número de palabras: ", numberWords)
+        
+        completed = 0;
+        for wordList in wordsList:
+            for word in wordList:
+                try:
+                    toSave = word.split(',')[0].rstrip()
+                    print(toSave)
+                    database.insertWord(toSave)
+                except:
+                    pass
+                completed+=1
+                print(str(completed)+"/"+str(numberWords)+"  --  "+str((completed/numberWords)*100)+"%", end="\r")
+
+def createDataBase():
+    '''Create DataBase'''
+    words = wordManage()
+    words.readAllFiles()
+    a = time.time()
+    words.getWordsList()
+    b = time.time()
+    result = b-a
+    print(result)
+
+class AutoComplete(object):
+    def __init__(self):
+        pass
+
+    def saveSentence(self, sentence):
+        '''
+        Guardar cada una de las palabras que tiene la oración
+        '''
+        sentence = sentence.lower()
+        newWords = sentence.split(" ")
+        database = db()
+        words = list()
+        words = database.getAllWords()#obtener las palabras actuales
+        actualNumberWords = len(words)#obtener el # de palabras actuales
+
+        newNumberWords = len(newWords)#obtener el # de palabras nuevas
+        
+        totalWords = list(set([*newWords, *words]))
+        newTotalWords = len(totalWords) #obtener el número total de palabras
+
+        for word in newWords:
+            toSave = re.sub('\W+', '', word) #palabra a guardar
+            try:
+                database.insertWord(toSave)
+                
+            except Exception as error:
+                #Si la palabra existe, sumar un uso
+                uses = database.getUsesWord(toSave)#Leer numero de usos
+                uses+=1#Sumar 1 uso
+                database.setUsesWord(toSave, uses)#Guardar numero de usos
+        
+        self.calculateAndSaveProb()
+
+    def calculateAndSaveProb(Self):
+        '''
+        Calcular y guardar la probabilidad de uso de cada palabra
+        '''
+        database = db()
+        words = database.getAllWords() #obtener todas las palabras en la base de datos
+        numberWords = len(words) #obtener su cantidad
+        for word in words:
+            uses = database.getUsesWord(word)
+            prob = uses/numberWords #Hacer probabilidad de uso
+            database.setProbWord(word, prob) #guardar probabilidad
+
+    def showOptions(self, initials):
+        '''
+        Mostrar las 3 primeras opciones
+        '''
+        database = db()
+        words = database.getWordInitials(initials)
+        opciones = ["", "", ""]
+        for i, option in enumerate(words[:3]):
+            opciones[i] = option[1]
+        
+        #Ordenar opciones por probabilidad
+        return opciones
+    
+    def listToSentence(self, lista):
+        sentence = str()
+        for word in lista:
+            sentence+=word+" "
+        return sentence
+
+#Escribir una oración
+#autocomplete = AutoComplete()
+
+#oracion = "Contenido no disponible"
+#autocomplete.saveSentence(oracion)
+
+#createDataBase()
---
+++ gui.py
@@ -0,0 +1,112 @@
+from PyQt5 import uic, QtWidgets, QtCore, Qt, QtGui
+from PyQt5.QtWidgets import (QApplication, QDialog, QPushButton, QTableWidget,
+                             QTableWidgetItem, QAbstractItemView, QHeaderView, QMenu,
+                             QActionGroup, QAction, QMessageBox, QInputDialog, QWidget,
+                             QFileDialog)
+
+from PyQt5.QtGui import QPainter, QColor, QFont
+import numpy.core._methods
+import numpy.lib.format
+import numpy as np
+import shutil
+import sys
+import os
+
+from autocomplete import AutoComplete
+
+qtCreatorFile = "gui.ui" # Nombre del archivo aquí.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    icons_path = "" 
+    def __init__(self, parent = None):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.setWindowTitle("AutoComplete")
+
+        #Oración
+        self.oracion = str()
+        self.list_words = list()
+        self.space = 0
+        self.AutoWord = str()
+
+        #Botones
+        self.aBtn.clicked.connect(lambda event: self.abcClick(1))
+        self.bBtn.clicked.connect(lambda event: self.abcClick(2))
+        self.cBtn.clicked.connect(lambda event: self.abcClick(3))
+        self.dBtn.clicked.connect(lambda event: self.abcClick(4))
+        self.eBtn.clicked.connect(lambda event: self.abcClick(5))
+        self.fBtn.clicked.connect(lambda event: self.abcClick(6))
+        self.gBtn.clicked.connect(lambda event: self.abcClick(7))
+        self.hBtn.clicked.connect(lambda event: self.abcClick(8))
+        self.iBtn.clicked.connect(lambda event: self.abcClick(9))
+        self.jBtn.clicked.connect(lambda event: self.abcClick(10))
+        self.kBtn.clicked.connect(lambda event: self.abcClick(11))
+        self.lBtn.clicked.connect(lambda event: self.abcClick(12))
+        self.mBtn.clicked.connect(lambda event: self.abcClick(13))
+        self.nBtn.clicked.connect(lambda event: self.abcClick(14))
+        self.nnBtn.clicked.connect(lambda event: self.abcClick(15))
+        self.oBtn.clicked.connect(lambda event: self.abcClick(16))
+        self.pBtn.clicked.connect(lambda event: self.abcClick(17))
+        self.qBtn.clicked.connect(lambda event: self.abcClick(18))
+        self.rBtn.clicked.connect(lambda event: self.abcClick(19))
+        self.sBtn.clicked.connect(lambda event: self.abcClick(20))
+        self.tBtn.clicked.connect(lambda event: self.abcClick(21))
+        self.uBtn.clicked.connect(lambda event: self.abcClick(22))
+        self.vBtn.clicked.connect(lambda event: self.abcClick(23))
+        self.wBtn.clicked.connect(lambda event: self.abcClick(24))
+        self.xBtn.clicked.connect(lambda event: self.abcClick(25))
+        self.yBtn.clicked.connect(lambda event: self.abcClick(26))
+        self.zBtn.clicked.connect(lambda event: self.abcClick(27))
+        self.spaceBtn.clicked.connect(lambda event: self.abcClick(28))
+        self.delBtn.clicked.connect(lambda event: self.abcClick(29))
+
+        self.newBtn.clicked.connect(self.newSentence)
+
+
+    def abcClick(self, number):
+        '''
+        Que letra se presionó
+        '''
+        abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+        autocomplete = AutoComplete()
+        if not number>28:
+            self.oracion += abc[number-1]
+            self.list_words = self.oracion.split(" ")
+            self.AutoWord = self.list_words[-1]
+            print(self.list_words, self.AutoWord)
+            
+        else:
+            if number==29: #borrar
+                self.oracion = self.oracion[:-1]
+                self.list_words = self.oracion.split(" ")
+                self.AutoWord = self.list_words[-1]
+                print(self.list_words, self.AutoWord)
+
+        self.plainTextEdit.clear()
+        self.plainTextEdit.insertPlainText(self.oracion)
+        #Buscar opciones para completar
+        autocomplete = AutoComplete()
+        words = autocomplete.showOptions(self.AutoWord)
+        self.word.setText(self.AutoWord)
+        opciones = list()
+        opsBtn = [self.op1, self.op2, self.op3]
+        for i, option in enumerate(words):
+            opciones.append(option)
+            opsBtn[i].setText(opciones[i])#Mostrar las opciones
+    
+    def newSentence(self):
+        print("Guardar oración")
+        self.plainTextEdit.clear()
+        autocomplete = AutoComplete()
+        autocomplete.saveSentence(self.oracion)
+        self.oracion = str()
+
+
+if __name__ == "__main__":
+    app =  QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
